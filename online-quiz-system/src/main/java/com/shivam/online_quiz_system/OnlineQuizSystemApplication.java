634b310299d3add3426247505b776263e6c45930
Shivam-2310 <nshivam2003@gmail.com>
2025-02-17 15:37:23 +0530

    Initial Project Setup

---
+++ online-quiz-system/src/main/java/com/shivam/online_quiz_system/OnlineQuizSystemApplication.java
@@ -0,0 +1,13 @@
+package com.shivam.online_quiz_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineQuizSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineQuizSystemApplication.class, args);
+	}
+
+}
